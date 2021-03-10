0210e478b75c0acc756ba073f52cfe27feba09d9
fritzmg <fmg@inspiredminds.at>
2021-03-10 16:27:31 +0000

    automatically add width and height

--- src/Controller/ContentElement/LottiePlayerController.php
+++ src/Controller/ContentElement/LottiePlayerController.php
@@ -20,6 +20,7 @@ use Contao\StringUtil;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Webmozart\PathUtil\Path;
 
 /**
  * @ContentElement(LottiePlayerController::TYPE, category="media")
@@ -27,6 +28,13 @@ use Symfony\Component\HttpFoundation\Response;
 class LottiePlayerController extends AbstractContentElementController
 {
     public const TYPE = 'lottie_player';
+
+    private $projectDir;
+
+    public function __construct(string $projectDir)
+    {
+        $this->projectDir = $projectDir;
+    }
 
     protected function getResponse(Template $template, ContentModel $model, Request $request): ?Response
     {
@@ -40,10 +48,24 @@ class LottiePlayerController extends AbstractContentElementController
             return new Response();
         }
 
+        $filepath = Path::join($this->projectDir, $file->path);
+
+        if (!is_file($filepath)) {
+            return new Response();
+        }
+
         $playerType = 'json' === $file->extension ? 'lottie' : 'tgs';
         $template->playerType = $playerType;
         $template->singleSRC = '/'.$file->path;
         $template->lottie_options = StringUtil::deserialize($model->lottie_options, true);
+
+        $animation = json_decode(file_get_contents($filepath));
+
+        if (!empty($animation->w) && !empty($animation->h)) {
+            $template->width = $animation->w;
+            $template->height = $animation->h;
+            $template->playerStyle = ' style="width:'.$animation->w.'px;height:'.$animation->h.'px"';
+        }
 
         if (empty($GLOBALS['TL_HEAD']['lottie-player-script'])) {
             $script = Template::generateScriptTag('bundles/contaolottieplayer/'.$playerType.'-player.js', true, null);
--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Contao Lottie Player extension.
+ *
+ * (c) inspiredminds
+ *
+ * @license LGPL-3.0-or-later
+ */
+
 use InspiredMinds\ContaoLottiePlayer\Controller\ContentElement\LottiePlayerController;
 
 $GLOBALS['TL_LANG']['CTE'][LottiePlayerController::TYPE] = ['Lottie Player', 'Plays Lottie or Telegram Sticker animations.'];
